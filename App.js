71906e295bde765914ad0e2d4b8eefb8fca5e280
afiqaimanafr <aaafr98@gmail.com>
2022-05-10 11:01:34 +0800

    update: latest

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
+import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Avatar } from "react-native-paper";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.headerContainer}>
         <Text>Header</Text>
+        <Avatar.Image size={200} source={require("./assets/favicon.png")} />
       </View>
       <View style={styles.bodyContainer}>
-        <Text>Body</Text>
+        <Text style={styles.loginText}>Login</Text>
       </View>
       <View style={styles.footerContainer}>
         <Text>Footer</Text>
@@ -24,6 +27,8 @@ const styles = StyleSheet.create({
   headerContainer: {
     flex: 0.5,
     backgroundColor: "yellow",
+    justifyContent: "center",
+    alignItems: "center",
   },
   bodyContainer: {
     flex: 0.4,
@@ -31,5 +36,9 @@ const styles = StyleSheet.create({
   },
   footerContainer: {
     flex: 0.1,
+  },
+  loginText: {
+    fontSize: 24,
+    paddingLeft: 12,
   },
 });
